653da19ae232355a9d8fd1b958932f41817e9749
AMP26 <amit.patnaik0126@gmail.com>
2025-07-09 20:26:25 +0530

    Bean Scopes

---
+++ 13-Bean-Lifecycle-Methods/beanlifecyclemethods/src/main/java/com/springboot/beanlifecyclemethods/paymentmethods/Bhim.java
@@ -0,0 +1,13 @@
+package com.springboot.beanlifecyclemethods.paymentmethods;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("bhim")
+public class Bhim implements Payment {
+    @Override
+    public String pay() {
+        return "BHIM Payment.";
+    }
+}
